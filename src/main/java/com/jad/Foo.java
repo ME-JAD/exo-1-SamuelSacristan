bc71163facb40ba91ce9855b555a1e65c19f4d01
SamuelSacristan <s.sacristan64@hotmail.com>
2025-06-10 10:11:42 +0200

    created all classes

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -1,4 +1,13 @@
 package com.jad;
 
 public class Foo {
+    private Bar bar;
+
+    public Foo(final Bar bar) {
+        this.bar = bar;
+    }
+
+    public Bar getBar() {
+        return this.bar;
+    }
 }
